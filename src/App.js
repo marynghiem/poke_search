628600a540a4189435ae4c8570abcf64b4012a61
marynghiem <marynghiem46@gmail.com>
2022-01-17 20:42:34 -0800

    Initiate poke search app with react routing and redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import "./App.css";
+import { Pokemon } from "./components/Pokemon";
+import { PokemonList } from "./components/PokemonList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<PokemonList />} />
+        <Route path="/pokemon/:pokemon" element={<Pokemon />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/PokemonList.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const PokemonList = () => <div>Pokemon List</div>;
