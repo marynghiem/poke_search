288a283f40db5198e696273c12bd53b40ea13c21
marynghiem <marynghiem46@gmail.com>
2022-01-18 16:47:28 -0800

    added pokemon image and showing data about pokemon from api

--- src/actions/pokemonActions.js
+++ src/actions/pokemonActions.js
@@ -18,3 +18,23 @@ export const GetPokemonList = (page) => async (dispatch) => {
     });
   }
 };
+
+export const GetPokemon = (pokemon) => async (dispatch) => {
+  console.log("getting pokemon");
+  try {
+    dispatch({
+      type: "POKEMON_MULTIPLE_LOADING",
+    });
+
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    dispatch({
+      type: "POKEMON_MULTIPLE_SUCCESS",
+      payload: res.data,
+      pokemonName: pokemon,
+    });
+  } catch (e) {
+    dispatch({
+      type: "POKEMON_MULTIPLE_FAIL",
+    });
+  }
+};
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,6 +1,47 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GetPokemon } from "../actions/pokemonActions";
+import _ from "lodash";
+import { useParams } from "react-router";
 
-export const Pokemon = (props) => {
-  console.log("props", props);
-  return <div>Pokemon</div>;
+export const Pokemon = () => {
+  const params = useParams();
+  const pokemonName = params.pokemon;
+  const dispatch = useDispatch();
+  const pokemonState = useSelector((state) => state.Pokemon);
+  React.useEffect(() => {
+    console.log("use efect");
+    dispatch(GetPokemon(pokemonName));
+  }, []);
+
+  const ShowData = () => {
+    if (!_.isEmpty(pokemonState.data[pokemonName])) {
+      console.log(1);
+      const pokeData = pokemonState.data[pokemonName];
+      return (
+        <div className={"pokemon-wrapper"}>
+          <div className={"item"}></div>
+          <h1>Sprites</h1>
+          <img src={pokeData.sprites.front_default} alt=""></img>
+        </div>
+      );
+    }
+    if (pokemonState.loading) {
+      console.log(2);
+      return <p>loading...</p>;
+    }
+    if (pokemonState.errorMsg !== "") {
+      console.log(3);
+      return <p>{pokemonState.errorMsg}</p>;
+    }
+    console.log(4);
+    return <p>error getting pokemon</p>;
+  };
+  console.log(5);
+  return (
+    <div>
+      <h1>{pokemonName}</h1>
+      {ShowData()}
+    </div>
+  );
 };
